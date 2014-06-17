c3553f8894893714c114dc602edd57872ce777a1
MrSourceCoded <sourcecodedcreations@gmail.com>
2014-06-18 06:32:24 +0800

    I didn't need to cache that

--- src/main/java/sourcecoded/strikeachord/eventsystem/events/MidiMessageScheduled.java
+++ src/main/java/sourcecoded/strikeachord/eventsystem/events/MidiMessageScheduled.java
@@ -2,5 +2,14 @@ package sourcecoded.strikeachord.eventsystem.events;
 
 import sourcecoded.strikeachord.eventsystem.IEvent;
 
+import javax.sound.midi.MidiMessage;
+
 public class MidiMessageScheduled implements IEvent {
+
+    public MidiMessage message;
+
+    public MidiMessageScheduled(MidiMessage message) {
+        this.message = message;
+    }
+
 }
